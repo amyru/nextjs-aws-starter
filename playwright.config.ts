9395d43c7102f2e2d1f19b3d40048446be52da14
Amy Castaldo <amyrudolph99@gmail.com>
2025-09-26 16:59:04 -0700

    adding environ files

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
   use: {
     baseURL: process.env.BASE_URL || 'http://localhost:3000',
   },
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
   webServer: {
-    command: 'npm run dev',
+    command: 'dotenv -e .env.test -- npm run dev',
     url: 'http://localhost:3000',
     reuseExistingServer: true,
-    timeout: 120_000
+    timeout: 120_000,
+    env: { NODE_ENV: 'test' }
   },
-  testDir: 'tests',
+  testDir: './tests',
 })
